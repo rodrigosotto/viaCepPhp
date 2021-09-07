22649764696ab0c26739ca6b31f2b4156403a063
jeffersontads <jeffersontads@hotmail.com>
2021-09-07 08:49:26 -0400

    feature: create index file for data entry

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+    require __DIR__.'/vendor/autoload.php';
 
+        //DEPENDENCIAS
+    use \App\WebService\ViaCep;
+
+    $dadosCEP = ViaCEP::consultarCEP("85854489");
+
+    print_r($dadosCEP);
 
 ?>
